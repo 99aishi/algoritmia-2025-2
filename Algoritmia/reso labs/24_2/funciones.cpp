0fe37bd752671c90eec35cbba0887bbaea585818
99aishi <00fabriciocordova@gmail.com>
2025-09-02 11:14:48 -0500

    update

---
+++ Algoritmia/reso labs/24_2/funciones.cpp
@@ -0,0 +1,5 @@
+//
+// Created by aishiteru on 2/09/2025.
+//
+
+#include "funciones.h"
---
+++ Algoritmia/reso labs/24_2/funciones.h
@@ -0,0 +1,8 @@
+//
+// Created by aishiteru on 2/09/2025.
+//
+
+#ifndef INC_24_2_FUNCIONES_H
+#define INC_24_2_FUNCIONES_H
+
+#endif //INC_24_2_FUNCIONES_H
