4e392c110ddb0ce9cdecab65be4f6d051e71efb4
99aishi <00fabriciocordova@gmail.com>
2025-09-09 12:55:41 -0500

    update

--- Algoritmia/reso labs/recu 25_1/funciones.cpp
+++ Algoritmia/reso labs/recu 25_1/funciones.cpp
@@ -10,7 +10,6 @@ void resultadoBusquedas(int matriz[6][7],int x,int y,int limX,int limY,int mov,c
     //caso base
 
     if (mov<0) return;
-
     if (matriz[x][y]==2 or x < 0 or x==limX or y<0 or y == limY) {
         if (direccion=='R') direccion = 'L';
         else if (direccion=='L') direccion = 'R';
@@ -19,32 +18,33 @@ void resultadoBusquedas(int matriz[6][7],int x,int y,int limX,int limY,int mov,c
     }else {
         //hacia abajo
         for (int i=x;i<limX;i++) {
+            if (matriz[i][y]==2) break;
             if (matriz[i][y]==1) {
-                cout << "Oro: "<< i <<" "<< y<<endl;
+                cout << "Oro: ("<< i <<","<< y<<")"<<endl;
                 break;
             }
         }
         //hacia arriba
         for (int i=x;i>=0;i--) {
+            if (matriz[i][y]==2) break;
             if (matriz[i][y]==1) {
-                cout << "Oro: "<< i<<" "<< y<<endl;
+                cout << "Oro: ("<< i<<","<< y<<")"<<endl;
                 break;
             }
         }
-        //
         //hacia derecha
         for (int i=y;i<limY;i++) {
+            if (matriz[i][y]==2) break;
             if (matriz[x][i]==1) {
-                cout << "Oro: "<< x<<" "<< i<<endl;
-
+                cout << "Oro: ("<< x<<","<< i<<")"<<endl;
                 break;
             }
         }
         //hacia izquierda
         for (int i=y;i>=0;i--) {
+            if (matriz[i][y]==2) break;
             if (matriz[x][i]==1) {
-
-                cout << "Oro: "<< x<<" "<< i<<endl;
+                cout << "Oro: ("<< x<<","<< i<<")"<<endl;
                 matriz[x][i]=0;
                 break;
             }
