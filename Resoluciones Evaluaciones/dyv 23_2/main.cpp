8402d25b6cbf2faafdb59b255f3c52707bb40121
unknown <00fabriciocordova@gmail.com>
2025-11-14 01:31:43 -0500

    update

---
+++ Resoluciones Evaluaciones/dyv 23_2/main.cpp
@@ -0,0 +1,61 @@
+
+
+#include <iostream>
+using namespace std;
+#define N 3
+#define M 10
+int max(int a, int b) {
+    return (a > b) ? a : b;
+}
+int buscaCentro(int * arr,int ini,int medio, int fin) {
+    int may=0;
+
+    for (int i=medio;i>=ini;i--) {
+        if (arr[i]>may) {
+            may=arr[i];
+        }
+    }
+    for (int i=medio;i<=fin;i++) {
+        if (arr[i]>may) {
+            may=arr[i];
+        }
+    }
+    return may;
+
+
+}
+int busca_mayor(int * arr,int ini,int fin) {
+
+    if (ini == fin) return arr[ini];
+    int medio = (ini + fin) / 2;
+    int izq = busca_mayor(arr,ini,medio);
+    int der = busca_mayor(arr,medio+1,fin);
+    int centro=buscaCentro(arr,ini,medio,fin);
+
+    return max(max(izq,der),centro);
+
+}
+int contar(int * arr,int ini,int fin,int buscar) {
+
+}
+int main(int argc, char **argv) {
+
+//NO TERMINADO
+    int matriz[N][M]={
+        {15,12,10,17,15,18,18,18,12,16},
+        {14,17,17,17,17,12,14,14,12,12},
+        {16,18,20,20,15,18,16,18,18,16}
+    };
+    int mayor=0;
+    for (int i = 0; i < N; i++) {
+        int mayor_local=busca_mayor(matriz[i],0,M-1);
+        if (mayor_local>mayor) {
+            mayor=mayor_local;
+        }
+
+    }
+    cout << mayor << endl;
+
+
+    return 0;
+}
